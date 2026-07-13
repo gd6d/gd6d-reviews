3a03602ed28ba396ec6ec07da8cf03ffb0a08c31
gd6d <olivier.gobet@gmail.com>
2026-07-13 14:58:53 +0200

    Correction de la largeur, des espacements et de la typographie

--- includes/class-gd6d-reviews-block.php
+++ includes/class-gd6d-reviews-block.php
@@ -12,6 +12,8 @@ final class GD6D_Reviews_Block {
 	public static function instance(): GD6D_Reviews_Block { if ( null === self::$instance ) { self::$instance = new self(); } return self::$instance; }
 	public function register(): void { add_action( 'init', array( $this, 'register_block' ) ); }
 	public function register_block(): void { register_block_type( GD6D_REVIEWS_DIR . 'blocks/reviews', array( 'render_callback' => array( $this, 'render' ) ) ); }
-	public function render( array $attributes = array() ): string { return GD6D_Reviews_Renderer::render( $attributes ); }
+	public function render( array $attributes = array() ): string {
+		return GD6D_Reviews_Renderer::render( $attributes );
+	}
 	private function __construct() {}
 }
